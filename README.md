6281bdb74e8685b29433e5d6c71b894d7f6397d7
Saulo Oliveira <santesoliveira@gmail.com>
2017-02-16 23:07:41 -0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # webpack-boilerplate
 Webpack boilerplate to kickstart your project
+
+##What's included?
+This boilerplate comes with the following features:
+- Dependency injection into your HTML file
+- SASS compiler & SASS linter
+- Twitter Bootstrap 3
+- Caching features (adding hash do filename and then only changes if the file's content changes)
+- jQuery
+- Babel
+
+##What do I need to change?
+1. Choose the name of your destination folder (this is the folder where all the bundles will be compiled into) and then rename the name dist to the name that you want.
+
+```
+output: {
+  	path: path.resolve(__dirname, '__dist__'),
+    filename: '[name].[chunkhash].js'
+  }
+```
+
+```
+devServer: {
+	contentBase: "./__dist__"
+}
+```
+
+2. Define the vendors that you will use and add then into your entry point.
+
+```
+entry: {
+	bundle: __dirname + '/src/index.js',
+	__vendor: ['babel-polyfill']__
+}
+```
+
+3. And finally rename your css bundle to the name that you want.
+
+```
+const extractSCSS = new ExtractTextPlugin('__styles.css__');
+```
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractSCSS = new ExtractTextPlugin('styles.css');
+
+module.exports = {
+  entry: {
+  	bundle: __dirname + '/src/index.js',
+  	vendor: ['babel-polyfill']
+  },
+  output: {
+  	path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$|.css$/,
+        loaders: extractSCSS.extract(["css-loader", "sass-loader"])
+      },
+      {
+        test: /\.scss$/,
+        exclude: /(node_modules|bower_components)/, 
+        loaders: ["sasslint-loader"]
+      },
+  		{ 
+  			test: /.jpe?g$|.gif$|.png$|.svg$|.woff$|.woff2$|.ttf$|.eot$/,
+  			loader: "url-loader" 
+  		},
+  		{
+  			test: /\.js$/,
+  			exclude: /(node_modules|bower_components)/,
+  			loader: 'babel-loader',
+  			query: {
+  				plugins: ['transform-runtime'],
+  				presets: ['es2015', 'stage-0', 'react']
+  			}
+  		}
+    ]
+  },
+  plugins: [
+  	extractSCSS,
+    new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new webpack.optimize.CommonsChunkPlugin({
+    	names: ['vendor', 'manifest']
+    }),
+  	new webpack.ProvidePlugin({   
+          jQuery: 'jquery',
+          $: 'jquery',
+          jquery: 'jquery'
+      })
+  ],
+	devServer: {
+		contentBase: "./dist"
+	},
+};
